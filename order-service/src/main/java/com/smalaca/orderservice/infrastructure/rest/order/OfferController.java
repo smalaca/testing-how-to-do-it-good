bc938ccc4bd934163eeec4628aa811d2a4020e3d
smalaca <ab13.krakow@gmail.com>
2020-09-11 19:31:16 +0200

    Dummy implementation of logic to look for offer to visualize removed dependency thanks to Boundary Object

--- order-service/src/main/java/com/smalaca/orderservice/infrastructure/rest/order/OfferController.java
+++ order-service/src/main/java/com/smalaca/orderservice/infrastructure/rest/order/OfferController.java
@@ -1,17 +1,27 @@
 package com.smalaca.orderservice.infrastructure.rest.order;
 
 import com.smalaca.orderservice.application.order.OfferItemDto;
+import com.smalaca.orderservice.application.order.OfferItemPriceDto;
+import com.smalaca.orderservice.infrastructure.warehouse.rest.ItemDto;
+import com.smalaca.orderservice.infrastructure.warehouse.rest.WarehouseRestClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/offer")
 public class OfferController {
+    private final WarehouseRestClient warehouseRestClient;
+
+    public OfferController(WarehouseRestClient warehouseRestClient) {
+        this.warehouseRestClient = warehouseRestClient;
+    }
+
     @GetMapping
     public List<OfferItemDto> find(@RequestParam String search) {
         /**
@@ -22,6 +32,14 @@ public class OfferController {
          *      - 15% in March and April
          * 3. Send information back to user
          */
-        return Collections.emptyList();
+        List<ItemDto> items = warehouseRestClient.findItems(search);
+
+        return items.stream()
+                .map(item -> new OfferItemDto(item.getId(), item.getName(), withDiscount(item)))
+                .collect(toList());
+    }
+
+    private OfferItemPriceDto withDiscount(ItemDto itemDto) {
+        return new OfferItemPriceDto(itemDto.getAmount(), itemDto.getCurrency());
     }
 }
