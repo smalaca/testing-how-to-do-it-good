b4b560c2ab90341d85471bed3b3487bdc457513f
smalaca <ab13.krakow@gmail.com>
2020-09-14 17:45:37 +0200

    Use OfferApplicationService in OfferController

--- order-service/src/main/java/com/smalaca/orderservice/infrastructure/rest/offer/OfferController.java
+++ order-service/src/main/java/com/smalaca/orderservice/infrastructure/rest/offer/OfferController.java
@@ -1,9 +1,7 @@
 package com.smalaca.orderservice.infrastructure.rest.offer;
 
+import com.smalaca.orderservice.application.offer.OfferApplicationService;
 import com.smalaca.orderservice.application.offer.OfferItemDto;
-import com.smalaca.orderservice.application.offer.OfferItemPriceDto;
-import com.smalaca.orderservice.infrastructure.warehouse.rest.ItemDto;
-import com.smalaca.orderservice.infrastructure.warehouse.rest.WarehouseRestClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,35 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
-
 @RestController
 @RequestMapping("/offer")
 public class OfferController {
-    private final WarehouseRestClient warehouseRestClient;
+    private final OfferApplicationService offerApplicationService;
 
-    public OfferController(WarehouseRestClient warehouseRestClient) {
-        this.warehouseRestClient = warehouseRestClient;
+    public OfferController(OfferApplicationService offerApplicationService) {
+        this.offerApplicationService = offerApplicationService;
     }
 
     @GetMapping
     public List<OfferItemDto> find(@RequestParam String search) {
-        /**
-         * 1. Get items from warehouse service.
-         * 2. Apply available discount:
-         *      - 10% if December
-         *      - 30% in September and October
-         *      - 15% in March and April
-         * 3. Send information back to user
-         */
-        List<ItemDto> items = warehouseRestClient.findItems(search);
-
-        return items.stream()
-                .map(item -> new OfferItemDto(item.getId(), item.getName(), withDiscount(item)))
-                .collect(toList());
-    }
-
-    private OfferItemPriceDto withDiscount(ItemDto itemDto) {
-        return new OfferItemPriceDto(itemDto.getAmount(), itemDto.getCurrency());
+        return offerApplicationService.find(search);
     }
 }
